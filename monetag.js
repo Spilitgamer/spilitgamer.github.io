4da437053f45d22215f75212673024fdb1dd4035
Spilitgamer <umerrafi1190@gmail.com>
2026-04-21 20:01:59 +0500

    Change service worker URL parameter to 'monetag'

--- monetag.js
+++ monetag.js
@@ -3,4 +3,4 @@ self.options = {
     "zoneId": 10906645
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=monetag')
